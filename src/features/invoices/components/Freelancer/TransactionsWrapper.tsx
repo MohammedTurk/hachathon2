65791b7d76fb98a998b7224017478462c1e9d663
souhair salah <sou6sal@gmail.com>
2023-03-25 13:53:31 +0300

    finaledit

--- src/features/invoices/components/Freelancer/TransactionsWrapper.tsx
+++ src/features/invoices/components/Freelancer/TransactionsWrapper.tsx
@@ -12,7 +12,7 @@ import { API_SERVICES_URLS } from "data";
 
 import { useSWRMutationHook } from "hooks";
 
-import { Send, Filter } from "components/svg";
+import { Send, Filter, Invoices } from "components/svg";
 
 import { MagnifyingGlassIconOutline, PlusIconMini } from "lib/@heroicons";
 
@@ -94,6 +94,38 @@ export const TransactionsWrapper = () => {
       {
         id: 7,
         value: "disapproved",
+        checked: false,
+      },
+    ],
+    Invoicesall: [
+      {
+        id: 1,
+        value: "paid",
+        checked: false,
+      },
+      {
+        id: 2,
+        value: "sent",
+        checked: false,
+      },
+      {
+        id: 3,
+        value: "Pending",
+        checked: false,
+      },
+      {
+        id: 4,
+        value: "Canceled",
+        checked: false,
+      },
+      {
+        id: 7,
+        value: "disapproved",
+        checked: false,
+      },
+      {
+        id: 8,
+        value: "refunded",
         checked: false,
       },
     ],
@@ -200,6 +232,7 @@ export const TransactionsWrapper = () => {
   const handleTabClick = (value: any) => {
     setType(value);
   };
+  console.log(type);
 
   return (
     <>
@@ -230,8 +263,9 @@ export const TransactionsWrapper = () => {
             <Filter />
             Filter
           </Button>
-
-          {open && (
+  
+          {type==="all" && (
+          open && (
             <Card className="z-10 absolute px-6 py-3 mt-10 ml-11">
               {checkboxes.invoice.map(({id,value,checked}) => (
                 <Checkbox
@@ -243,14 +277,43 @@ export const TransactionsWrapper = () => {
                   onClick={(e)=>handleCheck(e,id)}
                 />
               ))}
-
-            
             </Card>
-          )}
+          ) )}
+        {type==="invoice" && (
+          open && (
+          <Card className="z-10 absolute px-6 py-3 mt-10 ml-11">
+            {checkboxes.Invoicesall.map(({id,value,checked}) => (
+            <Checkbox
+              key={id}
+              id={value}
+              label={value.toUpperCase()}
+              checked={checked}
+              value={value}
+              onClick={(e)=>handleCheck(e,id)}
+            />
+          ))}
+        </Card>
+        ))}  
+        {type==="service" && (
+          open && (
+          <Card className="z-10 absolute px-6 py-3 mt-10 ml-11">
+            {checkboxes.link.map(({id,value,checked}) => (
+            <Checkbox
+              key={id}
+              id={value}
+              label={value.toUpperCase()}
+              checked={checked}
+              value={value}
+              onClick={(e)=>handleCheck(e,id)}
+            />
+          ))}
+        </Card>
+        ))} 
+
         </div>
       </div>
       <div>
-        <TabTable
+        <InvoicesTable
           types={["all", "invoice", "service"]}
           handleTabClick={handleTabClick}
           transactions={transactions}
@@ -268,3 +331,36 @@ export const TransactionsWrapper = () => {
   );
 };
  
+
+
+ {/* if(type==="all"){
+             open && (
+              <Card className="z-10 absolute px-6 py-3 mt-10 ml-11">
+                {checkboxes.invoice.map(({id,value,checked}) => (
+                  <Checkbox
+                    key={id}
+                    id={value}
+                    label={value.toUpperCase()}
+                    checked={checked}
+                    value={value}
+                    onClick={(e)=>handleCheck(e,id)}
+                  />
+                ))}
+              </Card>
+            )}
+            else if(type===invoice){
+              open && (
+                <Card className="z-10 absolute px-6 py-3 mt-10 ml-11">
+                  {checkboxes.link.map(({id,value,checked}) => (
+                    <Checkbox
+                      key={id}
+                      id={value}
+                      label={value.toUpperCase()}
+                      checked={checked}
+                      value={value}
+                      onClick={(e)=>handleCheck(e,id)}
+                    />
+                  ))}
+                </Card>
+              ) 
+            } */}
