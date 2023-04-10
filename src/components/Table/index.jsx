2fb597ede8d55e3f4be7bc921ac0551510dc5b74
mohammed <moh.saad.abu.kmail@gmail.com>
2023-04-10 20:31:09 +0300

    save

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,11 +1,49 @@
-import { Card, Triangle, IconButton } from "components";
+import { Card, Triangle, IconButton, Divider } from "components";
 import { useState } from "react";
 
-export const Table = ({ className = "", headers = [], onSort }) => {
+export const Table = ({
+  headers = [],
+  onSort,
+  data = [],
+  renderRow,
+  className = "",
+  thClassName = "",
+}) => {
   const [sortBaseOn, setSortBaseOn] = useState([null, false]);
 
-  function handleSortBaseOn(...rest) {
-    setSortBaseOn(rest);
+  function handleSortBaseOn(name) {
+    setSortBaseOn(name);
+    onSort(name);
+  }
+
+  const dividerTable = (
+    <tr>
+      <td colSpan={data[0].length}>
+        <Divider className="w-full !mb-0" />
+      </td>
+    </tr>
+  );
+
+  function getTh(head) {
+    return (
+      <div className="flex gap-3">
+        <span>{head}</span>
+        <span className="flex flex-col gap-[2px]">
+          <button onClick={() => handleSortBaseOn(head)}>
+            <Triangle
+              className={`w-3 h-3 ${head == sortBaseOn ? "text-black" : ""}`}
+            />
+          </button>
+          <button onClick={() => handleSortBaseOn(`-${head}`)}>
+            <Triangle
+              className={`w-3 h-3 rotate-180 ${
+                `-${head}` == sortBaseOn ? "text-black" : ""
+              }`}
+            />
+          </button>
+        </span>
+      </div>
+    );
   }
 
   return (
@@ -13,24 +51,41 @@ export const Table = ({ className = "", headers = [], onSort }) => {
       <table className="w-full">
         <thead>
           <tr>
-            {headers.map((head) => (
-              <th key={head}>
-                <div className="flex justify-evenly">
-                  <span>{head}</span>
-                  <span className="flex flex-col gap-[2px]">
-                    <button onClick={() => handleSortBaseOn(head, true)}>
-                      <Triangle className="w-3 h-3" />
-                    </button>
-                    <button onClick={() => handleSortBaseOn(head, false)}>
-                      <Triangle className="w-3 h-3 rotate-180" />
-                    </button>
-                  </span>
-                </div>
-              </th>
-            ))}
+            {headers.map((head) =>
+              typeof head == "string" ? (
+                <th
+                  key={head}
+                  className={`text-gray-400 font-medium  ${thClassName}`}
+                >
+                  {getTh(head)}
+                </th>
+              ) : (
+                <th
+                  key={head}
+                  className={`text-gray-400 flex gap-4 font-medium ${thClassName}`}
+                >
+                  {head.map((subhead) => getTh(subhead))}
+                </th>
+              )
+            )}
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {/* {data.map((row) => {
+            return (
+              <tr
+                onClick={rowOnClick}
+                role="button"
+                className="w-full pt-2 border-b cursor-pointer border-gray hover:bg-gray-light"
+              >
+                {renderRow(row)}
+              </tr>
+            );
+          })} */}
+          {/* w-full xl:w-[1000px] order-2 xl:order-1 h-[calc(100vh-190px)] ml-0  */}
+          {dividerTable}
+          {data.map((row) => renderRow(row))}
+        </tbody>
       </table>
     </Card>
   );
--- src/pages/testTable/index.jsx
+++ src/pages/testTable/index.jsx
@@ -2,11 +2,47 @@ import React from "react";
 import Table from "../../components/Table";
 
 export const testTable = () => {
+  const onClick = console.log;
+
+  function handleRender(row, tdClassName, ...rest) {
+    return (
+      <tr
+        className="w-full text-gray-900 bg-white border-b cursor-pointer border-gray hover:bg-gray-light text"
+        onClick={() => onClick(row)}
+        {...rest}
+      >
+        <td>
+          <div className="flex flex-col px-2 py-3 ">{row[0]}</div>
+        </td>
+        <td>
+          <div className="flex flex-col px-2 py-3 ">{row[1]}</div>
+        </td>
+        <td>
+          <div className="flex flex-col px-2 py-3 ">{row[2]}</div>
+        </td>
+      </tr>
+    );
+  }
   return (
     <Table
-      className="w-[600px]"
-      headers={["Name", "Date", "Amount", "Client", "Status"]}
-      onSort={(baseon, isUp) => console.log("baseon", baseon, isUp)}
+      className="w-[600px] "
+      thClassName="text-blue"
+      headers={[["Name", "Date"], "Amount", ["Client", "Status"]]}
+      onSort={(baseon) => console.log("baseon", baseon)}
+      data={[
+        ["ahmed", "2000", "300"],
+        ["ahmed", "2000", "300"],
+        ["ahmed", "2000", "300"],
+        [
+          "ahmed",
+          "2000",
+          "300",
+          {
+            dsadsa: "das",
+          },
+        ],
+      ]}
+      renderRow={handleRender}
     />
   );
 };
